8b528c963e57b4851e6694d6b7e777e9d56de827
SharonDeva <96017263+SharonDeva@users.noreply.github.com>
2024-10-30 21:10:28 +0530

    just start hero section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,9 +16,27 @@ const Nav = () => {
             className='m-0 w-[129px] h-[29px]'
           />
         </a>
-        <ul className='flex-1 justify-center items-center gap-16 max-lg:hidden'>
-            
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className='font-montserrat leading-normal text-lg text-slate-gray'
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
         </ul>
+
+        <div className='hidden max-lg:block'>
+          <img src={hamburger}
+               alt='Hamburger'
+               width={25}
+               height={25}
+          />
+        </div>
+
     </nav>
    </header>
   )
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,10 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section
+    id="home"
+    className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-2 border-red-500 p-2">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:bg-clip-padding-x pt-28"></div>
+    </section>
   )
 }
 
