6ec4eae82c97269b70665f786cd4f727e05f59b7
SharonDeva <96017263+SharonDeva@users.noreply.github.com>
2024-10-31 21:09:14 +0530

    just start button

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Button = ({label,iconURL}) => {
+  return (
+    <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white'>
+        {label}
+        
+        5e<img
+          src={iconURL} alt='arrow right icon'className='ml-2 rounded-full bg-white w-5 h-5'
+        />
+    </button>
+  )
+}
+
+export default Button
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -30,11 +30,7 @@ const Nav = () => {
         </ul>
 
         <div className='hidden max-lg:block'>
-          <img src={hamburger}
-               alt='Hamburger'
-               width={25}
-               height={25}
-          />
+          <img src={hamburger} alt='Hamburger'width={25} height={25} />
         </div>
 
     </nav>
