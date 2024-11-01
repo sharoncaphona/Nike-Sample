a843dce21a08572d1a4662605ac71bfb9fded896
SharonDeva <96017263+SharonDeva@users.noreply.github.com>
2024-11-01 22:34:36 +0530

    Add some in hero section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,9 +5,7 @@ const Button = ({label,iconURL}) => {
     <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white'>
         {label}
         
-        5e<img
-          src={iconURL} alt='arrow right icon'className='ml-2 rounded-full bg-white w-5 h-5'
-        />
+        <img src={iconURL} alt='arrow right icon'className='ml-2 rounded-full bg-white w-5 h-5'/>
     </button>
   )
 }
