5a75917deba4f0fe235cc1ec26715b91abc302c9
SharonDeva <96017263+SharonDeva@users.noreply.github.com>
2024-11-02 22:25:21 +0530

    just add shoecard component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,8 @@
 import Button from "../components/Button"
 import { arrowRight } from "../assets/icons"
-import {statistics} from "../constants";
+import {shoes,statistics} from "../constants";
 import { bigShoe1 } from "../assets/images";
+import ShoeCard from "../components/ShoeCard"
 
 const Hero = () => {
   return (
@@ -14,14 +15,12 @@ const Hero = () => {
           Our Summer Collection
         </p>
 
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
-          <span className="xl:bg-whit xl:whitespace-nowrap relative z-10 pr-10">
-            The New Arrivals
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
+            The New Arrival
           </span>
-          <br/>
-          <span className="text-coral-red inline-block mt-3 ">
-            Nike 
-          </span> Shoes
+          <br />
+          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
         </h1>
 
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm"> 
@@ -41,8 +40,19 @@ const Hero = () => {
 
       </div>
 
-      <div className="relative flex justify-center items-center ">
+      <div className="relative flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img src={bigShoe1} width={610} height={500} className="object-contain relative z-10" />
+        <div>
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard 
+              imageURL={shoe}
+              changeBigShoeImage={()=>{}}
+              bigShoeImag=""
+              />
+            </div>
+          ))}
+        </div>
       </div>
 
     </section>
